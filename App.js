ce59c95110c7a73ee4666772b36340bcedd3a615
thecaptain420 <mads130f@edu.easj.dk>
2020-02-19 17:07:46 +0100

    cleaned up

--- App.js
+++ App.js
@@ -5,14 +5,11 @@ import WelcomePage from './src/frontpage/WelcomePage'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
   }
 
   render() {
     return (
       <View>
-        <Text> App </Text>
         <WelcomePage/>
       </View>
     );
