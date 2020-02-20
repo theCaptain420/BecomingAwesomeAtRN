2b614b92a60678b77b7a1e53a1f57a70be2034ff
thecaptain420 <mads130f@edu.easj.dk>
2020-02-20 19:45:38 +0100

    added store to app

--- index.js
+++ index.js
@@ -5,5 +5,22 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React,{ Component } from 'react';
+import { Provider } from 'react-redux'
 
-AppRegistry.registerComponent(appName, () => App);
+import configureStore from './src/mainRedux/reduxStore'
+
+const store = configureStore()
+
+class Index extends Component {
+    render(){
+        return (
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        )
+    }
+
+}
+
+AppRegistry.registerComponent(appName, () => Index);
