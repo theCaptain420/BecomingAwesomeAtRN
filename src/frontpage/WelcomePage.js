11e4be2d97be80c8e5f808368139fc5816305d2a
thecaptain420 <mads130f@edu.easj.dk>
2020-02-22 16:58:00 +0100

    cleanup in code

--- src/frontpage/WelcomePage.js
+++ src/frontpage/WelcomePage.js
@@ -4,13 +4,16 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { LOADING_DATA, SUCCESS_LOADING_DATA, FAILED_LOADING_DATA }  from './frontRedux/frontTypes'
 
+import getAppData from './logic/getAppData'
+
 export default () => {
     const dispatch = useDispatch()
-    const errorMessage = useSelector(state => state.errorMessage)
+    const datamessage = useSelector(state => state.data)
+
     return(
         <View>
-            <Text onPress={()=>dispatch({type:LOADING_DATA})}>
-                TSM {errorMessage}
+            <Text onPress={()=>getAppData()}>
+                TSM {datamessage}
             </Text>
         </View>
     )
