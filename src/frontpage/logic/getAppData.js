7acced254ac711aeb4a55833e2f73fe39637153b
thecaptain420 <mads.nielsen@hotmail.com>
2020-02-27 17:26:10 +0100

    added get amount from array, needs to be tested

--- src/frontpage/logic/getAppData.js
+++ src/frontpage/logic/getAppData.js
@@ -4,9 +4,22 @@ export default async function () {
   try {
     const retrievedItem = await fetch("https://hacker-news.firebaseio.com/v0/newstories.json");
     const items = await retrievedItem.json()
-    console.log(items)
     return Promise.resolve([SUCCESS_LOADING_DATA, items]);
   } catch (error) {
     return Promise.reject([FAILED_LOADING_DATA, error])
   }
+}
+
+function getArticles(){
+
+}
+
+function getAmountOfArticleNumbers(itemArray, amount, startingPos){
+  let finalArray = []
+  for(let i = startingPos; i<amount; i++){
+      finalArray.push(itemArray[i]);
+  }
+
+  return finalArray;
+
 }
