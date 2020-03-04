7e92993a80c905f0cbe630203dddab1e6f46eb30
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-04 20:01:27 +0100

    implemented news elements

--- src/frontpage/WelcomePage.js
+++ src/frontpage/WelcomePage.js
@@ -2,6 +2,7 @@ import { View, Text } from "react-native"
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import styles from './frontPageStyle';
+import NewsElementsPrinter from './NewsComponents/NewsElementsPrinter'
 
 
 export default () => {
@@ -12,9 +13,9 @@ export default () => {
         <View style={styles.background}>
             <Text style={styles.mainHeader}>
                 Awesome APP  
-                TSM {articleList[0].by}
             </Text>
             <View style={[styles.seperatorLine,{width: "95%",}]}></View>
+            <NewsElementsPrinter items={articleList}/> 
         </View>
     )
 }
