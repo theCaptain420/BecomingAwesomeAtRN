24be4de33f44d0ce82ddd803e1c281d704f19f7c
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-04 20:02:05 +0100

    created news style

---
+++ src/frontpage/NewsComponents/newsStyle.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    newsBackground: {
+        backgroundColor:"#F7F7F7",
+    },
+    newsTitle: {
+        color:"#595959",
+        fontWeight:"bold",
+        fontSize:15,
+        margin:10
+    },
+    newsText: {
+        alignSelf:"center",
+        color:"#595959",
+        fontWeight:"bold",
+        fontSize:10,
+        margin:10
+    },
+    seperatorLine: {
+        backgroundColor:"lightgrey",
+        height:1,
+        width: "90%",
+        alignSelf:"center"
+    }
+});
