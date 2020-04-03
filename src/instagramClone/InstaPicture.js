c32df090aa63fbcacb04329198760d181d5e1a1c
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-03 12:19:01 +0200

    picture styled

--- src/instagramClone/InstaPicture.js
+++ src/instagramClone/InstaPicture.js
@@ -1,14 +1,18 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet,Image } from "react-native"
 import React from 'react';
+import { Dimensions } from 'react-native'
 
 
 export default (props) => {
-
     return(
-        <View >
-            <Text>
-                {props.short} lel
-            </Text>
+        <View style={{flex:1, marginVertical:30}}>
+            <Image style={styles.instaImage} source={{uri:props.imageURI}}/>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    instaImage:{
+        width:"100%",
+        height:Dimensions.get('window').width,
+    }
+})
