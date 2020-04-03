5b490f16e4a2e450b3bf2491252188fd78e90be1
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-03 12:18:56 +0200

    picture implemented

--- src/instagramClone/InstaElement.js
+++ src/instagramClone/InstaElement.js
@@ -1,13 +1,14 @@
 import { View, Text } from "react-native"
 import React from 'react';
 import InstaUser from './InstaUser'
+import InstaPicture from './InstaPicture'
 
 export default (props) => {
 
     return(
-        <View style={{flex:1}}>
+        <View style={{flex:1, flexDirection:"column"}}>
             <InstaUser fullName="Johnny Hansen" shortName="JH" pfpColor="red" pfpSize={35}/>
-           
+            <InstaPicture imageURI="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"/>
         </View>
     )
 }
