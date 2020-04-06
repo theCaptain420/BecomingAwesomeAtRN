ae4849fca26f44b4e2294d029806ca2e8dd77ae3
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-06 11:04:37 +0200

    implemented header in hacker news

--- src/frontpage/WelcomePage.js
+++ src/frontpage/WelcomePage.js
@@ -3,17 +3,14 @@ import React from 'react';
 import { useSelector } from 'react-redux'
 import styles from './frontPageStyle';
 import NewsElementsPrinter from './NewsComponents/NewsElementsPrinter'
-
+import AppHeader from '../themeComponents/AppHeader'
 
 export default () => {
     const articleList = useSelector(state => state.data)
 
     return (
         <View style={styles.background}>
-            <Text style={styles.mainHeader}>
-                Hacker News
-            </Text>
-            <View style={[styles.seperatorLine, { width: "95%", }]}></View>
+            <AppHeader title="Hacker News"/>
             <NewsElementsPrinter items={articleList} />
         </View>
     )
