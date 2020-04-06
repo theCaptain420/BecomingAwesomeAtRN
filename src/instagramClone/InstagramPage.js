50978e5693eda6e5a0c87974708fafaf1b57e21a
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-06 11:23:59 +0200

    created fake posts and implemented printer

--- src/instagramClone/InstagramPage.js
+++ src/instagramClone/InstagramPage.js
@@ -1,14 +1,19 @@
 import { View, Text } from "react-native"
 import React from 'react';
-import InstaElement from './InstaElement'
+import InstaPrinter from './instaPrinter'
 import AppHeader from '../themeComponents/AppHeader'
 
 export default ({navigation}) => {
+    const instaPosts=[
+        {fullName:"John Johnson", shortName:"JJ", pfpColor:"red", imageURI:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", postID:"1234"},
+        {fullName:"Lenny Denny", shortName:"LD", pfpColor:"green", imageURI:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", postID:"456"},
+        {fullName:"Diana Svendson", shortName:"DS", pfpColor:"blue", imageURI:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", postID:"852"}
+    ]
 
     return (
         <View>
             <AppHeader title="Instagram"/>
-            <InstaElement/>
+            <InstaPrinter items={instaPosts}/>
         </View>
     )
 }
