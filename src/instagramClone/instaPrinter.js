c3dba12170261cfbe27c106570c38454a356d40b
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-06 11:38:32 +0200

    implemented and created appfooter

--- src/instagramClone/instaPrinter.js
+++ src/instagramClone/instaPrinter.js
@@ -1,6 +1,7 @@
 import { ScrollView } from "react-native"
 import React from 'react';
 import InstaElement from './InstaElement'
+import AppFooter from '../themeComponents/AppFooter'
 
 export default (props) => {
     return(
@@ -10,6 +11,7 @@ export default (props) => {
                     <InstaElement key={item.postID} fullName={item.fullName} shortName={item.shortName} pfpColor={item.pfpColor} imageURI={item.imageURI}/>
                 )
             })}
+            <AppFooter/>
         </ScrollView>
     )
 }
---
+++ src/themeComponents/AppFooter.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from "react-native"
+import React from 'react';
+
+
+export default (props) => {
+    return(
+        <View >
+            <Text style={styles.mainFooter}>
+                Owned by @theCaptain420
+            </Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    mainFooter: {
+        alignSelf:"center",
+        color:"#595959",
+        fontWeight:"bold",
+        fontSize:10,
+        margin:10
+    }
+})
