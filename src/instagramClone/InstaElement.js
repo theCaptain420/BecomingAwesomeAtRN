ee78de431970cacf6d8f1d29d00dd87a86cf5e84
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-06 11:23:28 +0200

    mulitple posts now a thing

--- src/instagramClone/InstaElement.js
+++ src/instagramClone/InstaElement.js
@@ -4,11 +4,10 @@ import InstaUser from './InstaUser'
 import InstaPicture from './InstaPicture'
 
 export default (props) => {
-
     return(
         <View style={{flex:1, flexDirection:"column"}}>
-            <InstaUser fullName="Johnny Hansen" shortName="JH" pfpColor="red" pfpSize={35}/>
-            <InstaPicture imageURI="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"/>
+            <InstaUser fullName={props.fullName} shortName={props.shortName} pfpColor={props.pfpColor} pfpSize={35}/>
+            <InstaPicture imageURI={props.imageURI}/>
         </View>
     )
 }
