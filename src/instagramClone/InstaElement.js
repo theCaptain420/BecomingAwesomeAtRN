6ba4db374db039d09fbb3af104320173c9e2cb6a
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-07 11:47:27 +0200

    implemented actionbar in insta

--- src/instagramClone/InstaElement.js
+++ src/instagramClone/InstaElement.js
@@ -2,12 +2,21 @@ import { View, Text } from "react-native"
 import React from 'react';
 import InstaUser from './InstaUser'
 import InstaPicture from './InstaPicture'
+import InstaActionBar from './ActionBar/InstaActionBar'
 
 export default (props) => {
+    const styles = {
+        seperatorLine: {
+
+        }
+    }
+    
     return(
         <View style={{flex:1, flexDirection:"column"}}>
             <InstaUser fullName={props.fullName} shortName={props.shortName} pfpColor={props.pfpColor} pfpSize={20}/>
             <InstaPicture imageURI={props.imageURI}/>
+            <InstaActionBar/>
+            <View></View>
         </View>
     )
 }
