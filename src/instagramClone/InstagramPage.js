338806887c74ca561a3c5f97b1f38f97a4b06dc7
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-09 16:48:04 +0200

    un implemented activityindicator

--- src/instagramClone/InstagramPage.js
+++ src/instagramClone/InstagramPage.js
@@ -5,8 +5,8 @@ import AppHeader from '../themeComponents/AppHeader'
 
 export default ({navigation}) => {
     const instaPosts=[
-        {fullName:"John Johnson", shortName:"JJ", pfpColor:"red", imageURI:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", postID:"1234"},
-        {fullName:"Lenny Denny", shortName:"LD", pfpColor:"green", imageURI:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", postID:"456"},
+        {fullName:"John Johnson", shortName:"JJ", pfpColor:"red", imageURI:"https://unsplash.it/id/641/600/600", postID:"1234"},
+        {fullName:"Lenny Denny", shortName:"LD", pfpColor:"green", imageURI:"https://unsplash.it/600/600", postID:"456"},
         {fullName:"Diana Svendson", shortName:"DS", pfpColor:"blue", imageURI:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", postID:"852"}
     ]
 
