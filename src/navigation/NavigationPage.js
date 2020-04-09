880bfbbcd288cf5d020803a027879618e8d6ddc6
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-09 17:29:55 +0200

    style for Navigation page

--- src/navigation/NavigationPage.js
+++ src/navigation/NavigationPage.js
@@ -1,14 +1,16 @@
 import { View, Text } from "react-native"
 import React from 'react';
-
+import styles from './NavigationPageStyle'
+import AppStatusBar from '../themeComponents/AppStatusBar'
 
 export default ({navigation}) => {
     return (
-          <View>
-            <Text onPress={() => navigation.navigate('Instagram')} >
+          <View style={styles.navBackground}>
+            <AppStatusBar/>
+            <Text onPress={() => navigation.navigate('Instagram')} style={styles.navHeader} >
             Instagram
             </Text>
-            <Text onPress={() => navigation.navigate('HackerNews')} >
+            <Text onPress={() => navigation.navigate('HackerNews')} style={styles.navHeader}>
             HackerNews
             </Text>
           </View>
---
+++ src/navigation/NavigationPageStyle.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    navBackground: {
+        backgroundColor:"#F7F7F7",
+        justifyContent: "space-around",
+        flex:1,
+        alignItems:"center"
+    },
+    navHeader:{
+        fontSize:26,
+        color:"#595959",
+        padding:100,
+        backgroundColor:"#66ECAD",
+        borderRadius:45,
+        borderWidth:7,
+        borderColor:"#44E5BA"
+    }
+});
