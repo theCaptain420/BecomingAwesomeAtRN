1b89b4a59d292be0d532fcdc0b24ee00b885f34f
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-09 17:49:51 +0200

    Combined apptheme package

---
+++ src/themeComponents/AppTheme.js
@@ -0,0 +1,10 @@
+import AFooter from './AppFooter'
+import AStatus from './AppStatusBar'
+import AHeader from './AppHeader'
+
+export{
+    AFooter,
+    AStatus,
+    AHeader
+}
+//import {AFooter, AStatus, AHeader} from ''
