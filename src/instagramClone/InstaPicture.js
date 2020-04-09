0c00ba7866f182b6b5bb73a656a3f617761234bb
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-09 16:47:08 +0200

    implemented state hooks

--- src/instagramClone/InstaPicture.js
+++ src/instagramClone/InstaPicture.js
@@ -1,12 +1,19 @@
-import { View, Text, StyleSheet,Image } from "react-native"
-import React from 'react';
+import { View, Text, StyleSheet,Image, ActivityIndicator } from "react-native"
+import React,{useState} from 'react';
 import { Dimensions } from 'react-native'
 
 
 export default (props) => {
+    const [loading, setLoading] = useState(true);
+
+
     return(
         <View style={{flex:1, marginVertical:0}}>
-            <Image style={styles.instaImage} source={{uri:props.imageURI}}/>
+        {loading &&(
+            <ActivityIndicator size={"large"}/>
+        )}
+            <Image style={styles.instaImage} source={{uri:props.imageURI}} onLoad={()=>{setLoading(false)}} />
+            
         </View>
     )
 }
