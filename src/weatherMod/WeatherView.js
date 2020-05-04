5a8a4ee6beb94b6529de704d587214e18eebaf13
thecaptain420 <mads.nielsen@hotmail.com>
2020-05-04 19:10:50 +0200

    weather mod setup

---
+++ src/weatherMod/WeatherView.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+import React from 'react';
+import styles from './WeatherViewStyle'
+
+export default ({navigation}) => {
+    return (
+          <View style={styles.navBackground}>
+            The weather today
+          </View>
+    )
+}
