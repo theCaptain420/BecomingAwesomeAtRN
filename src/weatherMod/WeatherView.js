f3e785ba4292814210f61ea6e3c207e1bb578ea4
thecaptain420 <mads.nielsen@hotmail.com>
2020-05-04 19:13:30 +0200

    fixed weather, forgot text

--- src/weatherMod/WeatherView.js
+++ src/weatherMod/WeatherView.js
@@ -3,9 +3,14 @@ import React from 'react';
 import styles from './WeatherViewStyle'
 
 export default ({navigation}) => {
+
+    
+
     return (
           <View style={styles.navBackground}>
-            The weather today
+            <Text>
+                The weather today
+            </Text>
           </View>
     )
 }
