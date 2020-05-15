b117b28608bc97d19ff870adb24f413323a9de51
thecaptain420 <mads.nielsen@hotmail.com>
2020-05-15 20:42:06 +0200

    Nav: tried out new styling

--- src/navigation/NavigationPageStyle.js
+++ src/navigation/NavigationPageStyle.js
@@ -3,17 +3,22 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     navBackground: {
         backgroundColor:"#F7F7F7",
-        justifyContent: "space-around",
+        justifyContent:"center",
         flex:1,
         alignItems:"center"
     },
-    navHeader:{
-        fontSize:26,
-        color:"#595959",
-        padding:100,
+    navElement:{
+        backgroundColor:"red",
+        width:"90%",
+        alignItems:"center",
         backgroundColor:"#66ECAD",
-        borderRadius:45,
-        borderWidth:7,
-        borderColor:"#44E5BA"
-    }
+        padding:10,
+        margin:10
+    },
+    navText:{
+        fontSize:26,
+        color:"white",
+        fontWeight:"bold"
+    },
+   
 });
