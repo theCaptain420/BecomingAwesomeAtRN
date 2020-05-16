68e5b4fbd5a10aee7de19c2f758987b12f099f46
thecaptain420 <mads.nielsen@hotmail.com>
2020-05-16 11:02:56 +0200

    weahter: rounded temperature

--- src/weatherMod/getWeather.js
+++ src/weatherMod/getWeather.js
@@ -3,11 +3,18 @@ import { LOADING_WEATHER_DATA,SUCCESS_WEATHER_LOADING_DATA,FAILED_WEATHER_LOADIN
 export default async function getAppWeather(){
 
     try{
-        let responseTemp = await fetch("https://www.metaweather.com/api/location/554890/").then(resp => resp.json()).then(respJson =>  respJson.consolidated_weather[0].the_temp)
+        let responseTemp = await fetch("https://www.metaweather.com/api/location/554890/")
+            .then(resp => resp.json())
+            .then(respJson => roundTemperature(respJson.consolidated_weather[0].the_temp))
         return [SUCCESS_WEATHER_LOADING_DATA, responseTemp]
     }catch(err){
         console.log(err)
         return [FAILED_WEATHER_LOADING_DATA,err]
     }
 
+}
+
+function roundTemperature(temperature){
+    let roundedTemperature = Math.round(temperature);
+    return roundedTemperature;
 }
