b26cfe4d06e80903cc5dd057280f02e858e9356f
thecaptain420 <mads.nielsen@hotmail.com>
2020-05-18 12:42:40 +0200

    test from new pc

--- src/instagramClone/InstaPFP.js
+++ src/instagramClone/InstaPFP.js
@@ -11,7 +11,7 @@ export default (props) => {
             height:props.size,
             justifyContent: 'center',
             alignItems:"center",
-            justifyContent:"center",
+            justifyContent:"center"
         },
         pfpText:{
             fontWeight:"bold",
