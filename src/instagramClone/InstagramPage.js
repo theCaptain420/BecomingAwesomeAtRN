ed841924773e4defabc864594d300af93fd8d741
thecaptain420 <mads.nielsen@hotmail.com>
2020-05-28 20:33:01 +0200

    created comments

--- src/instagramClone/InstagramPage.js
+++ src/instagramClone/InstagramPage.js
@@ -5,9 +5,9 @@ import AppHeader from '../themeComponents/AppHeader'
 
 export default () => {
     const instaPosts=[
-        {fullName:"John Johnson", shortName:"JJ", pfpColor:"red", imageURI:"https://unsplash.it/id/646/600/600", postID:"1234"},
-        {fullName:"Lenny Denny", shortName:"LD", pfpColor:"green", imageURI:"https://unsplash.it/600/600", postID:"456"},
-        {fullName:"Diana Svendson", shortName:"DS", pfpColor:"blue", imageURI:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", postID:"852"}
+        {fullName:"John Johnson", shortName:"JJ", pfpColor:"red", imageURI:"https://unsplash.it/id/646/600/600", postID:"1234",comments:[{comment:"tsm"},{comment:"clg"}]},
+        {fullName:"Lenny Denny", shortName:"LD", pfpColor:"green", imageURI:"https://unsplash.it/600/600", postID:"456",comments:[{comment:"skrrt"},{comment:"clg"}]},
+        {fullName:"Diana Svendson", shortName:"DS", pfpColor:"blue", imageURI:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", postID:"852",comments:[{comment:"lol"},{comment:"clg"}]}
     ]
 
     return (
