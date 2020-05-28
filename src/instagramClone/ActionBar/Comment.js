ad4344e7a4d5c515d5cfeda92be049f8d830c8eb
thecaptain420 <mads.nielsen@hotmail.com>
2020-05-28 20:16:40 +0200

    the new comment modal

---
+++ src/instagramClone/ActionBar/Comment.js
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet, Modal } from "react-native"
+import React, { useState } from 'react';
+
+
+export default (props) => {
+
+    return (
+        <View >
+            <Modal animationType="slide"
+                transparent={false}
+                visible={props.modalVisibility}
+                onRequestClose={() => {
+                    props.toggleModal
+                }}>
+
+                <Text>
+                    Comments
+                </Text>
+            </Modal>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+
+})
