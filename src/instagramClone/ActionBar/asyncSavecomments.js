ea25b8be3276eef718bc13b935a5cdd70e4f8e15
thecaptain420 <mads.nielsen@hotmail.com>
2020-06-02 12:47:35 +0200

    now can save json values async

--- src/instagramClone/ActionBar/asyncSavecomments.js
+++ src/instagramClone/ActionBar/asyncSavecomments.js
@@ -3,22 +3,27 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 async function storeData(value) {
     try {
-      await AsyncStorage.setItem('@storage_Key', value)
+        //turns json into string before saving, as it can only save as string.
+        const jsonValue = JSON.stringify(value)
+
+        await AsyncStorage.setItem('@storage_Key', jsonValue)
     } catch (e) {
         console.log(e)
     }
 }
 
-async function getData () {
+async function getData() {
     try {
-      const value = await AsyncStorage.getItem('@storage_Key')
-      console.log(value)
-      if(value !== null) {
-        // value previously stored
-      }
-    } catch(e) {
-      console.log(e)
+        const value = await AsyncStorage.getItem('@storage_Key')
+
+        //es6 to check if it isnt null, and then turns it into json
+        value != null ? JSON.parse(value) : null; 
+
+        console.log(value)
+        
+    } catch (e) {
+        console.log(e)
     }
 }
 
-export {getData,storeData}
+export { getData, storeData }
